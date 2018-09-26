d6c02c0d599081aae0a35d813b4ec58f31b70090
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-09-26 12:41:13 -0500

    CG 11-1

--- StudentNames/Program.cs
+++ StudentNames/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace StudentNames
 {
@@ -6,7 +8,25 @@ namespace StudentNames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> students = new List<Student>();
+            string line;
+            int numberOfGrades = 0;
+            StreamReader file = new StreamReader("C:\\Users\\melissa\\source\\repos\\StudentNames\\StudentNames\\TextFile1.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] lines = line.Split(" ");
+                numberOfGrades = lines.Length;
+                string studentName = lines[0];
+                Student newStudent = new Student(studentName, numberOfGrades);
+                students.Add(newStudent);
+            }
+            foreach (Student newStudent in students)
+            {
+                if (newStudent.Grades >=7)
+                    Console.WriteLine(newStudent.Name);
+                
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ StudentNames/student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentNames
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Grades { get; set; }
+
+        public Student(string name, int grades)
+        {
+            Name = name;
+            Grades = grades;
+        }
+     }
+}
